1f56a3864f5fe337b7deb1cee2a5dc4afa3d7ccc
Mahmudul-hasan16 <m.hasan5979@gmail.com>
2022-10-30 23:07:56 +0600

    firebase and download pdf setup

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -2,38 +2,38 @@ import React, { createContext, useEffect, useState } from 'react';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile, signInWithPopup } from "firebase/auth";
 import app from '../firebase/firebase.init';
 
-export const ProfileContext =createContext()
-const auth =getAuth(app);
-const UserContext = ({children}) => {
-    const [loading,setLoading] = useState(true)
+export const ProfileContext = createContext()
+const auth = getAuth(app);
+const UserContext = ({ children }) => {
+    const [loading, setLoading] = useState(true)
     // this is email and password signUp function
-    const register = (email,password) =>{
+    const register = (email, password) => {
         setLoading(true)
-        return createUserWithEmailAndPassword(auth,email,password)
+        return createUserWithEmailAndPassword(auth, email, password)
     }
     // this is email and password signIn function
-    const logIn = (email,password) =>{
+    const logIn = (email, password) => {
         setLoading(true)
-        return signInWithEmailAndPassword(auth,email,password)
+        return signInWithEmailAndPassword(auth, email, password)
     }
     // this is  signOut function
-    const logOut = ()=>{
+    const logOut = () => {
         setLoading(true)
         return signOut(auth);
     }
     // this user data 
-    const [user ,setUser] = useState(null)
-    useEffect(()=>{
-        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+    const [user, setUser] = useState(null)
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             setLoading(false);
         })
-    return () =>{
-        unsubscribe()
-    }
-    },[])
+        return () => {
+            unsubscribe()
+        }
+    }, [])
     // this profile update function
-    const changeProfile =(name , img)=>{
+    const changeProfile = (name, img) => {
         return updateProfile(auth.currentUser, {
             displayName: name,
             photoURL: img
@@ -41,12 +41,12 @@ const UserContext = ({children}) => {
     }
     // this google and github pop up signUp system
     const popUpSignIn = (provider) => {
-        return signInWithPopup(auth,provider)
+        return signInWithPopup(auth, provider)
     }
     // course enroll data 
-    const [enroll,setEnroll] = useState([])
+    const [enroll, setEnroll] = useState([])
 
-    const authInfo ={register,logIn,logOut,user,changeProfile,popUpSignIn,enroll,setEnroll ,loading};
+    const authInfo = { register, logIn, logOut, user, changeProfile, popUpSignIn, enroll, setEnroll, loading };
     return (
         <ProfileContext.Provider value={authInfo}>
             {children}
