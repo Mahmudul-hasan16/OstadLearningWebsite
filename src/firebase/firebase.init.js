4631408396fc3cc189b04a379a78774eec723218
Mahmudul-hasan16 <m.hasan5979@gmail.com>
2022-11-01 01:11:28 +0600

    firebase modified

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAAlZQUcDxqIMT564GZTq6BgAhbJPOKNec",
-    authDomain: "ostad-ede0c.firebaseapp.com",
-    projectId: "ostad-ede0c",
-    storageBucket: "ostad-ede0c.appspot.com",
-    messagingSenderId: "290695529344",
-    appId: "1:290695529344:web:aedc35fee2fadfa45f7765"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
